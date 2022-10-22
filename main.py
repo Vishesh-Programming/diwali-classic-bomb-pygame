f5549683d32d56b79c1fe2d59ba5002738928a1b
Vishesh Pandey <116044800+visheshpandey2001@users.noreply.github.com>
2022-10-22 18:16:01 +0000

    handled explosion and added danger zone

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ window = pygame.display.set_mode([500,500])
 
 pygame.display.set_caption("Code Olympics")
 
+font = pygame.font.SysFont("Times New Roman",10)
+bigFont = pygame.font.SysFont("Times New Roman" , 40)
+
+# colors
+red=(200,14,14,0.2)
+blue = (0,0,200)
+green = (144,238,144)
+black = (0,0,0)
+
+winningText = font.render("Congratulations! You made it out safely. Have a safe and happy diwali.",False, black)
+losingText = font.render("You were too close to cracker. Please maintain proper distance when using crackers.",False, black)
+runText = bigFont.render("Run Run ....",False, red)
 # images 
 boy = pygame.image.load("boy.png")
 boy = pygame.transform.scale(boy,(100,100))
@@ -51,6 +63,8 @@ class TNT:
         self.y = y
         self.center_x = self.x + 50
         self.center_y = self.y + 50
+        self.explosion_initiated = False
+        self.explosion = False
 
     def move_forward(self):
         self.x -= 10   
@@ -71,14 +85,17 @@ class TNT:
     def display(self):
         window.blit(self.tnt , (self.x,self.y))
 
-    def explosion(self):
+    def initiate_explosion(self):
+        self.explosion_initiated = True
+
+    def doExplosion(self):
         self.tnt = explosion
+        self.explosion = True
+        
         
 tnt = TNT(100,350)
-        
-# colors
-red=(200,0,0)
-blue = (0,0,200)
+tntCountDown = 180
+    
 
 clock = pygame.time.Clock()
 
@@ -87,9 +104,11 @@ run = True
 # game loop
 while run:
     clock.tick(60)
-    window.fill(red)
+    window.fill(green)
+    pygame.draw.rect(window,red,pygame.Rect(0,250,300,300),5)
     player.display()
     tnt.display()
+  
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -109,11 +128,24 @@ while run:
         print("player x" , player.x)
         print("tnt x" , tnt.x) 
         print("-")
-        print(abs(player.center_x - tnt.center_x))
-        print(abs( player.center_y - tnt.center_y))
-        print("-")
         if abs(player.x - tnt.center_x) <= 20 and abs( player.y - tnt.center_y) <= 20 :
             print("In range")
-            tnt.explosion()
+            tnt.initiate_explosion()
+            
+
+    if tnt.explosion_initiated == True :
+        window.blit(runText , (20 , 50))
+        tntCountDown -= 1 
+        if tntCountDown < 0 :
+            tnt.doExplosion()
+
+    if tnt.explosion == True :
+        if player.x > 300 or player.y < 250 :
+            pass  # display winning 
+            window.blit(winningText,(20,100))
+
+        else :
+            pass # display loss
+            window.blit(losingText, (20,100))
         
     pygame.display.update()
